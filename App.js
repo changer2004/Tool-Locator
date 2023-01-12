54829a965aafa2689bc443590ef738eab83a27c6
changer2004 <changer2004@users.noreply.github.com>
2023-01-12 10:00:09 +0000

    Robert
    Kolejna zmiana

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! To jest test</Text>
+      <Text>ROBERT test</Text>
       <StatusBar style="auto" />
     </View>
   );
